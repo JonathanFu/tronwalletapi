1dc72cb7ed515af1abdcd4965c073f96916a132b
Jonathan Fu <fujianglai@gmail.com>
2018-05-09 03:27:32 +1000

    Test one api

--- app.js
+++ app.js
@@ -8,25 +8,25 @@ app.get('/accounts', async (req, res) => {
   res.json(await Account.loadAccounts());
 });
 
-app.get('/tokenBalances', async (req, res) => {
-    res.json(await Account.loadTokenBalances(req.query.password));
-});
-
-app.get('/allBlocks', async (req, res) => {
-    res.json(await Account.loadAllBlocks());
-});
-
-app.get('/nodes', async (req, res) => {
-    res.json(await Network.loadNodes());
-});
-
-app.get('/witnesses', async (req, res) => {
-    res.json(await Network.loadWitnesses());
-});
-
-app.get('/tokens', async (req, res) => {
-    res.json(await Tokens.loadTokens());
-});
+// app.get('/tokenBalances', async (req, res) => {
+//     res.json(await Account.loadTokenBalances(req.query.password));
+// });
+//
+// app.get('/allBlocks', async (req, res) => {
+//     res.json(await Account.loadAllBlocks());
+// });
+//
+// app.get('/nodes', async (req, res) => {
+//     res.json(await Network.loadNodes());
+// });
+//
+// app.get('/witnesses', async (req, res) => {
+//     res.json(await Network.loadWitnesses());
+// });
+//
+// app.get('/tokens', async (req, res) => {
+//     res.json(await Tokens.loadTokens());
+// });
 
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
